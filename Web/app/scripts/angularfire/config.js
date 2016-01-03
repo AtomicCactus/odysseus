093c41f521107c27ec9ab22a42b0d8c2e5aa8c3d
Kristopher Knabel <krisk@insideradius.com>
2016-01-03 00:32:22 -0800

    Created Web Boilerplate

---
+++ Web/app/scripts/angularfire/config.js
@@ -0,0 +1,2 @@
+angular.module('firebase.config', [])
+  .constant('FBURL', 'https://intense-inferno-4940.firebaseio.com');
---
+++ Web/app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name odysseusApp
+ * @description
+ * # odysseusApp
+ *
+ * Main module of the application.
+ */
+angular.module('odysseusApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref'
+  ]);
